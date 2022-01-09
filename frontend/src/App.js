06c8d5dc0d472d402aed8c2ba2fe969ef31e6d3e
lironbard <lironbard@gmail.com>
2022-01-09 23:51:22 +0200

    Updating App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,77 +1,46 @@
-// import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import React, { useState, useEffect } from "react";
-import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import SideBar from "./components/SideBar";
-import Footer from "./components/Footer";
-import HomeScreen from "./screens-pages/HomeScreen";
-import PetScreen from "./screens-pages/PetScreen";
-import LogIn from "./components/LogInModal";
-import SignUp from "./components/SignUpModal";
-import Search from "./components/Search";
+import Header from "./components/header/Header";
+import HomePage from "./components/homepage/HomePage";
+import MyPets from "./components/myPets/MyPets";
+import Search from "./components/search/Search";
+import ProfileSettings from "./components/profileSettings/ProfileSettings";
+import AuthProvider, { useAuth } from "./context/auth";
+import PetDescription from "./components/petDescription/PetDescription";
+import AddPet from "./components/adminPages/AddPet";
+import Dashboard from "./components/adminPages/Dashboard";
+import AllPetsDisplay from "./components/adminPages/AllPetsDisplay";
+import AllUsersDisplay from "./components/adminPages/AllUsersDisplay";
+import DisplayUser from "./components/adminPages/DisplayUser";
+
+const AppRouter = () => {
+  let auth = useAuth();
+  if (!auth.isInitiallyLoaded) {
+    return <div></div>;
+  }
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/settings/:userId" render={(routeProps) => <ProfileSettings {...routeProps} />} />
+        <Route exact path="/pet/:petId" render={(routeProps) => <PetDescription {...routeProps} />} />
+        <Route exact path="/dashboard" render={(routeProps) => <Dashboard {...routeProps} />} />
+        <Route exact path="/dashboard/allpets" render={(routeProps) => <AllPetsDisplay {...routeProps} />} />
+        <Route exact path="/dashboard/allusers" render={(routeProps) => <AllUsersDisplay {...routeProps} />} />
+        <Route exact path="/dashboard/user/:userId" render={(routeProps) => <DisplayUser {...routeProps} />} />
+        <Route exact path="/pet" render={(routeProps) => <AddPet {...routeProps} />} />
+        <Route exact path="/pet/user/:userId" render={(routeProps) => <MyPets {...routeProps} />} />
+        <Route exact path="/search" render={(routeProps) => <Search {...routeProps} />} />
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+    </Router>
+  );
+};
 
 function App() {
-  const [petsArray, setPetsArray] = useState("");
-
   return (
-    <>
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={() => (
-                <>
-                  <SideBar />
-                  <HomeScreen />
-                  <Footer />
-                </>
-              )}
-            />
-            <Route path="/Search">
-              <SideBar />
-              <Search />
-              <Footer />
-            </Route>
-            <Route path="/pet/:id">
-              <SideBar />
-              <PetScreen />
-              <Footer />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </>
-    // <Router>
-    //   <div className="App">
-    //     <NavBar />
-    //     <Switch>
-    //       <Route
-    //         exact
-    //         path="/"
-    //         render={() => (
-    //           <main>
-    //             <Container>
-    //               {/* <Form />
-    //             <List /> */}
-    //               <h1>Hi</h1>
-    //               <Footer />
-    //             </Container>
-    //           </main>
-    //         )}
-    //       />
-    //       <Route path="/Login">
-    //         <LogIn />
-    //       </Route>
-    //       <Route path="/Signin">
-    //         <SignUp />
-    //       </Route>
-    //     </Switch>
-    //   </div>
-    // </Router>
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
   );
 }
 
